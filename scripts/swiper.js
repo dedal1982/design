4d9d0d0386ffb09703ca68bd0f8873667143ea1a
AlDm <d44.a1@yandex.ru>
2023-10-07 02:02:21 +0300

    6

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -94,7 +94,7 @@ const swiperL = new Swiper(".license__swiper", {
       spaceBetween: 8,
     },
     375: {
-      slidesPerView: 1.1,
+      slidesPerView: 1.5,
       spaceBetween: 10,
     },
     768: {
@@ -107,6 +107,51 @@ const swiperL = new Swiper(".license__swiper", {
     },
     1920: {
       slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+/*слайдер секкции артикл*/
+const swiper3 = new Swiper(".articles__swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".articles__swiper-button-next",
+    prevEl: ".articles__swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 8,
+    },
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2.4,
+      spaceBetween: 10,
+    },
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1920: {
+      slidesPerView: 3,
       spaceBetween: 30,
     },
   },
