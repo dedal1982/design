cb4a2a7f2292ed606082c2e8589f9fac6a0e7224
AlDm <d44.a1@yandex.ru>
2023-10-06 23:48:29 +0300

    5

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -71,3 +71,47 @@ const makeCodeUniversal = (
 };
 
 makeCodeUniversal(".completed__tab", ".completed__swiper");
+
+/*слайдер лицензий*/
+const swiperL = new Swiper(".license__swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 5,
+  spaceBetween: 18,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".license__swiper-button-prev",
+    prevEl: ".license__swiper-button-next",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 8,
+    },
+    375: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3.7,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1920: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
