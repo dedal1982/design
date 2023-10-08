51486f3b513030612172147da35dd1ff51a21360
AlDm <d44.a1@yandex.ru>
2023-10-09 02:35:34 +0300

    222

--- scripts/index.js
+++ scripts/index.js
@@ -1,103 +1,126 @@
-// const makeCodeUniversal = (
-//   tabItemsQuery,
-//   formItemsQuery,
-//   tabClassName = "active"
-// ) => {
-//   const tabItems = Array.from(document.querySelectorAll(tabItemsQuery));
-//   const formItems = Array.from(document.querySelectorAll(formItemsQuery));
+const makeCodeUniversal = (
+  tabItemsQuery,
+  formItemsQuery,
+  tabClassName = "active"
+) => {
+  const tabItems = Array.from(document.querySelectorAll(tabItemsQuery));
+  const formItems = Array.from(document.querySelectorAll(formItemsQuery));
 
-//   const clearActiveTabs = (element) => {
-//     element.find((item) => item.classList.remove(tabClassName));
-//   };
+  const clearActiveTabs = (element) => {
+    element.find((item) => item.classList.remove(tabClassName));
+  };
 
-//   const setActiveTab = (element, index) => {
-//     element[index].classList.add(tabClassName);
-//   };
+  const setActiveTab = (element, index) => {
+    element[index].classList.add(tabClassName);
+  };
 
-//   const chekTab = (item, index) => {
-//     item.addEventListener("click", () => {
-//       clearActiveTabs(tabItems);
-//       clearActiveTabs(formItems);
+  const chekTab = (item, index) => {
+    item.addEventListener("click", () => {
+      clearActiveTabs(tabItems);
+      clearActiveTabs(formItems);
 
-//       setActiveTab(tabItems, index);
-//       setActiveTab(formItems, index);
-//     });
-//   };
-//   tabItems.forEach(chekTab);
-// };
-// // /*табы / секция Наши реализованные проекты*/
-// makeCodeUniversal(".completed__tab", ".completed__swiper");
+      setActiveTab(tabItems, index);
+      setActiveTab(formItems, index);
+    });
+  };
+  tabItems.forEach(chekTab);
+};
+// /*табы / секция Наши реализованные проекты*/
+makeCodeUniversal(".completed__tab", ".completed__swiper");
 
-// /*переключение табов(городов) в секции карта*/
-// makeCodeUniversal(".salons__tab", ".salons__stations");
+/*переключение табов(городов) в секции карта*/
+makeCodeUniversal(".salons__tab", ".salons__stations");
 
-// /*переключение табов(выбор кухни) в секции to-choose*/
-// makeCodeUniversal(".to-choose__icon", ".to-choose__content");
-// makeCodeUniversal(".to-choose__mobile-icon", ".to-choose__mobile-inner");
-// makeCodeUniversal(".to-choose__mobile-icon", ".mobile-title");
+/*переключение табов(выбор кухни) в секции to-choose*/
+makeCodeUniversal(".to-choose__icon", ".to-choose__content");
+makeCodeUniversal(".to-choose__mobile-icon", ".to-choose__mobile-inner");
+makeCodeUniversal(".to-choose__mobile-icon", ".mobile-title");
 
-// function loadYouTubeVideo() {
-//   var youtubeVideoDiv = document.getElementById("youtubeVideo");
-//   youtubeVideoDiv.innerHTML =
-//     '<iframe clacc="iframe-yt" width="100%" height="100%" src="https://www.youtube.com/embed/oLY1rss9Jx8?si=-5eVgEZEd3nRIRP0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-// }
-// function loadYouTubeVideo2() {
-//   var youtubeVideoDiv = document.getElementById("youtubeVideoTwo");
-//   youtubeVideoDiv.innerHTML =
-//     '<iframe width="100%" height="100% " src="https://www.youtube.com/embed/oLY1rss9Jx8?si=-5eVgEZEd3nRIRP0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-// }
-/*******************/
+function loadYouTubeVideo() {
+  var youtubeVideoDiv = document.getElementById("youtubeVideo");
+  youtubeVideoDiv.innerHTML =
+    '<iframe clacc="iframe-yt" width="100%" height="100%" src="https://www.youtube.com/embed/oLY1rss9Jx8?si=-5eVgEZEd3nRIRP0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}
+function loadYouTubeVideo2() {
+  var youtubeVideoDiv = document.getElementById("youtubeVideoTwo");
+  youtubeVideoDiv.innerHTML =
+    '<iframe width="100%" height="100% " src="https://www.youtube.com/embed/oLY1rss9Jx8?si=-5eVgEZEd3nRIRP0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+}
+/*слайдер до и после*/
+const slider = document.querySelector(".slider");
+const before = document.querySelector(".before");
+const beforeImage = before.querySelector("img");
+const change = document.querySelector(".change");
+const body = document.body;
 
-(function () {
-  var dragMe = document.getElementsByClassName("dragme")[0],
-    container = document.getElementsByClassName("sl-container")[0],
-    viewAfter = document.getElementsByClassName("view-after")[0];
+let isActive = false;
 
-  dragMe.addEventListener("mousemove", function (e) {
-    e.preventDefault();
-    var initialX = e.clientX,
-      initialLeft = parseFloat(getComputedStyle(dragMe).left);
+document.addEventListener("DOMContentLoaded", () => {
+  let width = slider.offsetWidth;
+  beforeImage.style.width = `${width}px`;
+});
 
-    document.addEventListener("mousemove", moveElement);
-    document.addEventListener("mouseup", releaseElement);
+change.addEventListener("mousedown", () => {
+  isActive = true;
+});
 
-    function moveElement(e) {
-      var moveX = e.clientX - initialX;
-      dragMe.style.left = initialLeft + moveX + "px";
-      viewAfter.style.width = initialLeft + moveX + 5 + "px";
-    }
+body.addEventListener("mouseup", () => {
+  isActive = false;
+});
 
-    function releaseElement() {
-      document.removeEventListener("mousemove", moveElement);
-      document.removeEventListener("mouseup", releaseElement);
-    }
-  });
+body.addEventListener("mouseleave", () => {
+  isActive = false;
+});
 
-  container.addEventListener("click", function (e) {
-    var containerLeft = container.getBoundingClientRect().left,
-      eventLeft = e.clientX - containerLeft - 15;
-    animateTo(eventLeft);
-  });
+const beforeAfterSlider = (x) => {
+  let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+  before.style.width = `${shift}px`;
+  change.style.left = `${shift}px`;
+};
 
-  animateTo("50%");
+const pauseEvents = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  return false;
+};
 
-  function animateTo(_left) {
-    var currentLeft = parseFloat(getComputedStyle(dragMe).left);
-
-    var id = setInterval(frame, 10);
-
-    function frame() {
-      if (currentLeft === _left) {
-        clearInterval(id);
-      } else {
-        if (currentLeft < _left) {
-          currentLeft++;
-        } else {
-          currentLeft--;
-        }
-        dragMe.style.left = currentLeft + "%";
-        viewAfter.style.width = currentLeft + "%";
-      }
-    }
+body.addEventListener("mousemove", (e) => {
+  if (!isActive) {
+    return;
   }
-})();
+
+  let x = e.pageX;
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(e);
+});
+
+change.addEventListener("touchstart", () => {
+  isActive = true;
+});
+
+body.addEventListener("touchend", () => {
+  isActive = false;
+});
+
+body.addEventListener("touchcancel", () => {
+  isActive = false;
+});
+
+body.addEventListener("touchmove", (e) => {
+  if (!isActive) {
+    return;
+  }
+
+  let x;
+
+  let i;
+  for (i = 0; i < e.changedTouches.length; i++) {
+    x = e.changedTouches[i].pageX;
+  }
+
+  x -= slider.getBoundingClientRect().left;
+
+  beforeAfterSlider(x);
+  pauseEvents(e);
+});
